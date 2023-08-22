31a7199682dd356375cb86a4d08bbba2c0fab382
mariogarzac <mariogarzac99@gmail.com>
2023-08-21 23:36:25 -0600

    Day 05

--- main.go
+++ main.go
@@ -1,38 +1,45 @@
 package main
 
 import (
-    "Advent/days/day01"
-    "Advent/days/day02"
-    "Advent/days/day03"
-    "Advent/days/day04"
-    "fmt"
+	"Advent/days/day01"
+	"Advent/days/day02"
+	"Advent/days/day03"
+	"Advent/days/day04"
+	"Advent/days/day05"
+	"fmt"
 )
 
 func main(){
 
     // day one
     fmt.Println("Day one")
-    fmt.Println(one.FindMax("days/days01/calories.txt"))
-    fmt.Println(one.FindTopThree("days/days01/calories.txt"))
+    fmt.Println(one.FindMax("day01/calories.txt"))
+    fmt.Println(one.FindTopThree("day01/calories.txt"))
     
     fmt.Println()
     
     // day two
     fmt.Println("Day two")
-    fmt.Println(two.GetMatchScore("days/days02/rps.txt"))
-    fmt.Println(two.GetStrategyScore("days/days02/rps.txt"))
+    fmt.Println(two.GetMatchScore("day02/rps.txt"))
+    fmt.Println(two.GetStrategyScore("day02/rps.txt"))
     
     fmt.Println()
-
+    
     // day three
     fmt.Println("Day three")
-    fmt.Println(three.RuckSack("days/days03/contents.txt"))
-    fmt.Println(three.RuckSackPart2("days/days03/contents.txt"))
+    fmt.Println(three.RuckSack("day03/contents.txt"))
+    fmt.Println(three.RuckSackPart2("day03/contents.txt"))
     
     fmt.Println()
-
+    
     //day four
     fmt.Println("Day four")
-    fmt.Println(four.Cleanup("days/days04/sections.txt"))
-    fmt.Println(four.Cleanup2("days/days04/sections.txt"))
+    fmt.Println(four.Cleanup("day04/sections.txt"))
+    fmt.Println(four.Cleanup2("day04/sections.txt"))
+    
+    //day five
+    fmt.Println("Day five")
+    fmt.Printf("%c ",five.OperateCrane("day05/boxes.txt"))
+    fmt.Println()
+
 }
