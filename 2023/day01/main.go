4ab3a0d52e6763365e6ff384e070ae8ac955c044
mariogarzac <mariogarzac99@gmail.com>
2023-12-02 11:35:17 -0600

    part 2: Changed the whole file to a more efficient method of finding numbers

--- 2023/day01/main.go
+++ 2023/day01/main.go
@@ -2,49 +2,78 @@ package main
 
 import (
     "fmt"
-    "strconv"
+    "log"
+    "strings"
 
     "github.com/mariogarzac/utils"
 )
 
-func part1() int{
-    sc, file, err := utils.OpenFile("day01/input.txt")
+var numberMap = map[string]string {
+    "one":   "one1one",
+    "two":   "two2two",
+    "three": "three3three",
+    "four":  "four4four",
+    "five":  "five5five",
+    "six":   "six6six",
+    "seven": "seven7seven",
+    "eight": "eight8eight",
+    "nine":  "nine9nine",
+}
+
+func main(){
+    sc, file, err := utils.OpenFile("input.txt")
 
     if err != nil {
-        return 0
+        log.Fatal(err)
     }
 
     defer file.Close()
 
-    var nums []byte
-    count := 0
+    countP1, countP2 := 0,0
 
     for sc.Scan(){
         line := sc.Text()
-
-        // get all the numbers in the string 
-        for i := range line{
-            if line[i] >= '0' && line[i] <= '9'{
-                nums = append(nums, line[i] - '0')
-            }
-
-        }
-
-        // get the first and the last byte from the slice and convert them to a string
-        num := strconv.Itoa(int(nums[0])) + strconv.Itoa(int(nums[len(nums) - 1 ]))
-
-        // convert the string back to an int lmao
-        result,_ := strconv.Atoi(num)
-
-        // add the number and reset the array
-        count += result
-        nums = nums[:0]
+        countP1 += part1(line)
+        countP2 += part2(line)
     }
 
-    return count
+    fmt.Println(countP1)
+    fmt.Println(countP2)
 }
 
-func main(){
-    fmt.Println(part1())
+func part1(line string) int{
+
+    var first,last byte
+    var foundFirst bool
+
+    foundFirst = false
+
+    fmt.Println(line)
+
+    // get all the numbers in the string 
+    for i := range line{
+        if line[i] >= '0' && line[i] <= '9'{
+            if !foundFirst {
+                first = line[i] - '0'
+                foundFirst = true
+            }
+            last = line[i] - '0'
+        }
+    }
+
+    fmt.Println(first, last)
+    return int(first * 10 + last)
 }
 
+func part2(line string) int{
+
+    for word, digit := range numberMap {
+        if strings.Contains(line, word){
+            line = strings.ReplaceAll(line, word, digit)
+        }
+    }
+
+    number := part1(line)
+
+    return number
+}
