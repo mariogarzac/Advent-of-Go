14f93580dc45a14ec1d8bbd330bca3a631c96d67
mariogarzac <mariogarzac99@gmail.com>
2023-12-09 23:51:21 -0600

    Part 1: Took me longer that in should have.

--- 2023/day05/main.go
+++ 2023/day05/main.go
@@ -1,30 +1,110 @@
-
 package main
 
 import (
-	"log"
+    "fmt"
+    "log"
+    "strconv"
+    "strings"
 
-	"github.com/mariogarzac/Advent/utils"
+    "github.com/mariogarzac/Advent/utils"
 )
 
+type Block struct {
+    destination int
+    source int
+    length int
+}
+
 func main() {
-	sc, file, err := utils.OpenFile("input.txt")
 
-     if err != nil {
-         log.Fatal(err)
-     }
+    file, err := utils.ReadWholeFile("input.txt")
 
-     defer file.Close()
-     
-     for sc.Scan(){
-         line := sc.Text()
-         part1(line)
-     }
+    if err != nil {
+        log.Fatal(err)
+    }
 
+
+    part1(string(file))
+}
+
+func parseBlock(block string) [][]int{ 
+
+    blockRanges := strings.Split(block, "\n")[1:]
+
+    numbers := [][]int{}
+
+    // process each individual map
+    for _,ranges := range blockRanges {
+
+        // skip any empty ranges
+        if ranges == ""{
+            continue
+        }
+
+        // split the range by spaces
+        n := []int{}
+        blockRange := strings.Fields(ranges)
+
+        // convert each number in the range to an int and append it
+        for _,r := range blockRange {
+            number,_ := strconv.Atoi(r)
+            n = append(n, number)
+        }
+
+        // append the number slice to the slice that will be returned
+        numbers = append(numbers, n)
+    }
+
+    return numbers
 }
 
 func part1(line string) int {
+    almanac := strings.Split(line, "\n\n")
+    seeds := parseSeeds(almanac[0])
+    blocks := almanac[1:]
 
-return 0
+    for _,block := range blocks {
+        maps := [][]int{}
+        splitRanges := parseBlock(block)
+        maps = splitRanges
+
+        newSeeds := []int{}
+        for _,s := range seeds{
+            found := false
+            for _, m := range maps {
+                srcStart, dstStart, rangeLen := m[0], m[1], m[2]
+                if dstStart <= s && s < dstStart + rangeLen{
+                    newSeeds = append(newSeeds, s - dstStart + srcStart)
+                    found = true
+                    break
+                }
+            }
+            if !found {
+                newSeeds = append(newSeeds, s)
+            }
+        }
+        seeds = newSeeds
+    }
+
+
+    minSeed := seeds[0]
+    for _, s := range seeds {
+        if s < minSeed{
+            minSeed = s
+        }
+    }
+    fmt.Println(minSeed)
+    return 0
 }
 
+func parseSeeds(s string) []int{
+    str := strings.Split(s, " ")[1:]
+    nums := make([]int,len(str))
+
+    for i,n := range str{
+        num,_ := strconv.Atoi(n)
+        nums[i] = num
+
+    }
+    return nums
+}
