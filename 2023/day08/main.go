4730c5bd1f1ef4d5271dbc1bf75fadb946daf8cd
mariogarzac <mariogarzac99@gmail.com>
2023-12-14 14:42:31 -0600

    Part 2: Thanks youtube

--- 2023/day08/main.go
+++ 2023/day08/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "regexp"
-    "strings"
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
 
-    "github.com/mariogarzac/Advent/utils"
+	"github.com/cznic/mathutil"
+
+	"github.com/mariogarzac/Advent/utils"
 )
 
 type Directions struct {
@@ -22,17 +24,19 @@ func main() {
         log.Fatal(err)
     }
 
-    steps, mappy := parseInstructions(string(file))
-    fmt.Println(part1(steps, mappy))
+    steps, mappy, count := parseInstructions(string(file))
+    // fmt.Println(part1(steps, mappy))
+    fmt.Println(part2(steps, mappy, count))
 
 }
 
-func parseInstructions(directions string) (string, map[string]Directions) {
+func parseInstructions(directions string) (string, map[string]Directions, []string) {
 
     lines := strings.Split(directions, "\n\n")
     steps := lines[0]
 
     mappy := map[string]Directions{}
+    starts := []string{}
 
     network := strings.Split(lines[1], "\n")
 
@@ -43,11 +47,14 @@ func parseInstructions(directions string) (string, map[string]Directions) {
         net := cleanLine(line)
         // remove double space and split
         n := strings.Split(strings.ReplaceAll(net, "  ", " "), " ")
+        if n[0][len(n) - 1] == 'A' {
+            starts = append(starts, n[0])
+        }
 
         mappy[n[0]] = Directions{ left: n[1], right: n[2] }
     }
 
-    return steps, mappy
+    return steps, mappy, starts
 }
 
 func part1(steps string, mappy map[string]Directions) int {
@@ -82,7 +89,54 @@ func part1(steps string, mappy map[string]Directions) int {
     return count
 }
 
+func part2(steps string, network map[string]Directions, positions []string) int {
+
+    cycles := [][]int{}
+    for _, curr := range positions {
+        cycle := []int{}
+
+        firstZ := ""
+
+        stepCount := 0
+        i := 0
+
+        for {
+            for stepCount == 0 || curr[len(curr) - 1] != 'Z' {
+                stepCount += 1
+                if steps[i] == 'L' {
+                    curr = network[curr].left
+                }else{
+                    curr = network[curr].right
+                }
+
+                i += 1
+
+                if i == len(steps) { i = 0 }
+
+            }
+
+            cycle = append(cycle, stepCount)
+
+            if firstZ == "" {
+                firstZ = curr
+            }else if firstZ == curr {
+                break
+            }
+        }
+
+        cycles = append(cycles, cycle)
+    }
+
+    lcm := cycles[0][0]
+
+    for _, nums := range cycles[1:] {
+        num := nums[0]
+        lcm = (lcm * num) / int(mathutil.GCDUint64(uint64(lcm), uint64(num)))
+    }
+
+    return lcm
+}
+
 func cleanLine(line string) string{
     return regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(line, "")
 }
-
